2e77068df211c1b7181b7aafa8698c034c535206
ZDD <ZDD@10.30.190.9>
2017-12-05 19:30:17 +0800

    更新controller

--- src/main/java/xmu/crms/view/TopicController.java
+++ src/main/java/xmu/crms/view/TopicController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package xmu.crms.view;
 
 import java.util.ArrayList;
@@ -22,7 +21,6 @@ public class TopicController {
 
 	//按ID获取话题，传入话题id，返回话题对象
 	@RequestMapping(value="/{topicId}", method=RequestMethod.GET)
-	@ResponseStatus(value=HttpStatus.OK)
 	public Topic getTopicById(@PathParam("topicId") Integer topicId){
 		Topic topic=new Topic();//假的
 		return topic;
@@ -31,15 +29,13 @@ public class TopicController {
 	//按ID修改话题，传入话题id和json
 	@RequestMapping(value="/{topicId}", method=RequestMethod.PUT)
 	@ResponseStatus(value=HttpStatus.CREATED)
-	public String updateTopicById(@PathParam("topicId") Integer topicId, @RequestBody Topic topic){
-		return "修改成功";
+	public void updateTopicById(@PathParam("topicId") Integer topicId, @RequestBody Topic topic){
 	}
 	
 	//按ID删除话题，传入话题id
 	@RequestMapping(value="/{topicId}", method=RequestMethod.DELETE)
 	@ResponseStatus(value=HttpStatus.NO_CONTENT)
-	public String deleteTopicById(@PathParam("topicId") Integer topicId){
-	    return "删除成功";
+	public void deleteTopicById(@PathParam("topicId") Integer topicId){
 	}
 	
 	//按话题ID获取选择了该话题的小组
@@ -50,56 +46,3 @@ public class TopicController {
 		return groupList;
 	}
 }
-=======
-package xmu.crms.view;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.websocket.server.PathParam;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import xmu.crms.model.Group;
-import xmu.crms.model.Topic;
-
-@RestController
-@RequestMapping("/topic")
-public class TopicController {
-
-	//按ID获取话题，传入话题id，返回话题对象
-	@RequestMapping(value="/{topicId}", method=RequestMethod.GET)
-	@ResponseStatus(value=HttpStatus.OK)
-	public Topic getTopicById(@PathParam("topicId") Integer topicId){
-		Topic topic=new Topic();//假的
-		return topic;
-	}
-
-	//按ID修改话题，传入话题id和json
-	@RequestMapping(value="/{topicId}", method=RequestMethod.PUT)
-	@ResponseStatus(value=HttpStatus.CREATED)
-	public String updateTopicById(@PathParam("topicId") Integer topicId, @RequestBody Topic topic){
-		return "修改成功";
-	}
-	
-	//按ID删除话题，传入话题id
-	@RequestMapping(value="/{topicId}", method=RequestMethod.DELETE)
-	@ResponseStatus(value=HttpStatus.NO_CONTENT)
-	public String deleteTopicById(@PathParam("topicId") Integer topicId){
-	    return "删除成功";
-	}
-	
-	//按话题ID获取选择了该话题的小组
-	@RequestMapping(value="/{topicId}/group", method=RequestMethod.GET)
-	@ResponseStatus(value=HttpStatus.OK)
-	public List<Group> getGroupListByTopicId(@PathParam("topicId") Integer topicId){
-		List<Group> groupList=new ArrayList<Group>();
-		return groupList;
-	}
-}
->>>>>>> branch 'master' of https://ddancn:zzz11009900@github.com/ddancn/CourseManageSystem.git
